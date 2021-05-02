cbc4f3d5b00fe9776b327b2eea17843b478068b5
Emmanuel Castillo <>
2021-05-02 20:41:48 +0000

    I solve some operation of math.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
+# two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
 #Write your code below this line 👇
-a = int(two_digit_number[0])
-b = int(two_digit_number[1])
+# a = int(two_digit_number[0])
+# b = int(two_digit_number[1])
 
-print(a + b)
+# print(a + b)
+
+print(3 * 3 + 3 / 3 - 3) #7.0 es el resultado.
 
 
+print(3 + 3 / 3 * 3 - 3) #Cambie el orden del codigo para que de como resultado 3.0
 
 
 
